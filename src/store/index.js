19c1c173b98cdb48d8ae2f818c4a0c172caad810
AyabulelaSityata <sityataayabulela@gmail.com>
2023-07-03 16:55:04 +0200

    done

--- src/store/index.js
+++ src/store/index.js
@@ -2,6 +2,11 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    skills: null,
+    education: null,
+    testimonials: null,
+    projects: null,
+
   },
   getters: {
   },
